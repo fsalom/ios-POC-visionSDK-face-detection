29a9c3a3666955419a5881cf46a132000bb011a8
Fernando Salom <fer@rudo.es>
2024-03-27 08:49:05 +0100

    Add initial project

---
+++ facedetector/facedetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  facedetectorApp.swift
+//  facedetector
+//
+//  Created by Fernando Salom Carratala on 26/3/24.
+//
+
+import SwiftUI
+
+@main
+struct facedetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
