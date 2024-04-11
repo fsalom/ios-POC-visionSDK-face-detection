12d5f25792ce0d7a096f4290684da38085016270
Fernando Salom <fer@rudo.es>
2024-04-10 23:42:29 -0400

    Update CameraPreviewView.swift

--- facedetector/presentation/module/camera/CameraPreviewView.swift
+++ facedetector/presentation/module/camera/CameraPreviewView.swift
@@ -13,14 +13,15 @@ struct CameraPreviewView: View {
             viewModel.preview?
                 .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
                 .edgesIgnoringSafeArea(.all)
-                .overlay(
+                .overlay(alignment: .bottomLeading, content: {
                     GeometryReader { reader in
                         ForEach(viewModel.faces) { face in
-                            Rectangle()
-                                .path(in: viewModel.calculateCGRect(with: face, and: reader))
-                                .stroke(Color.red, lineWidth: 2.0)
+                             Rectangle()
+                             .path(in: viewModel.calculateCGRect(with: face, and: reader))
+                             .stroke(Color.red, lineWidth: 2.0)
                         }
-                    })
+                    }
+                })
 
             VStack {
                 Spacer()
