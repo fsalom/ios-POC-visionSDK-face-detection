6efe6fb043b95eabbb83ae65b2161b852ed1b5bc
Fernando Salom <fer@rudo.es>
2024-04-13 08:40:06 -0400

    Add bindings to try to update preview with real time modification

--- facedetector/presentation/module/camera/component/Previewer.swift
+++ facedetector/presentation/module/camera/component/Previewer.swift
@@ -4,13 +4,16 @@ import AVFoundation
 struct Preview: UIViewControllerRepresentable {
     let previewLayer: AVCaptureVideoPreviewLayer
     let gravity: AVLayerVideoGravity
+    var drawings: Binding<[CAShapeLayer]>
 
     init(
         session: AVCaptureSession,
-        gravity: AVLayerVideoGravity
+        gravity: AVLayerVideoGravity,
+        drawings: Binding<[CAShapeLayer]>
     ) {
         self.gravity = gravity
         self.previewLayer = AVCaptureVideoPreviewLayer(session: session)
+        self.drawings = drawings
     }
 
     func makeUIViewController(context: Context) -> UIViewController {
@@ -23,6 +26,9 @@ struct Preview: UIViewControllerRepresentable {
         uiViewController.view.layer.addSublayer(previewLayer)
 
         previewLayer.frame = uiViewController.view.bounds
+        drawings.forEach { faceBoundingBox in
+            uiViewController.view.layer.addSublayer(faceBoundingBox.wrappedValue)
+        }
         print("updated")
     }
 
